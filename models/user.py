55faa042823bd7a6e201a92a5518ec7bb7b713e5
MatheusVinicius77 <matheusviniciusbus7@hotmail.com>
2024-06-06 08:59:33 -0300

    fix: add user model after adding config in env.py

---
+++ models/user.py
@@ -0,0 +1,11 @@
+from database.database import Base
+from sqlalchemy import Column, Integer,String
+
+
+class UserModel(Base):
+    __tablename__= "users"
+
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True)
+    hashed_password = Column(String)
