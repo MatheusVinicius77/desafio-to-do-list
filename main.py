2801cfb07e3cc031f931a179351308f4802b9331
MatheusVinicius77 <matheusviniciusbus7@hotmail.com>
2024-06-05 22:36:05 -0300

    feat: create tables in postgresql

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+import uvicorn
+from database.database import Base,engine
+# from models.task import Task
+
+
+Base.metadata.drop_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-
-@app.get("/")
-async def home() -> dict[str,str]:
-    return {"status":"Api running"}
+if __name__ == '__main__':
+    uvicorn.run("main:app", port=3000, reload=True)
