c085539df1951c0e4c2ad49054e6d5077b7e901f
MatheusVinicius77 <matheusviniciusbus7@hotmail.com>
2024-06-06 09:10:34 -0300

    feat: add root api rout

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI
 import uvicorn
 from database.database import Base,engine
 from models.task import TaskModel
-from routes.task import router
-
+from routes.task import router as task_router
+from routes.auth import router as auth_router, default_router
 # Code bellow are commented because deletes the database
 # data all the time. Just for development
 
@@ -13,7 +13,9 @@ from routes.task import router
 app = FastAPI()
 
 
-app.include_router(router, prefix='')
+app.include_router(task_router, prefix='')
+app.include_router(auth_router, prefix='')
+app.include_router(default_router, prefix='')
 
 if __name__ == '__main__':
     uvicorn.run("main:app", port=3000, reload=True)
