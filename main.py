a88210a0de69587b527648e4eefa70a954b77ea2
MatheusVinicius77 <matheusviniciusbus7@hotmail.com>
2024-06-06 03:10:32 -0300

    feat: add routes

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
 import uvicorn
 from database.database import Base,engine
-# from models.task import Task
+from models.task import TaskModel
+from routes.task import router
 
+# Code bellow are commented because deletes the database
+# data all the time. Just for development
 
-Base.metadata.drop_all(bind=engine)
-Base.metadata.create_all(bind=engine)
+# Base.metadata.drop_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+app.include_router(router, prefix='')
 
 if __name__ == '__main__':
     uvicorn.run("main:app", port=3000, reload=True)
---
+++ routes/task.py
@@ -0,0 +1,48 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from typing import List, Dict
+
+from schemas.task import TaskSchema, TaskCreateSchema, TaskUpdateSchema
+from services import task
+from database.db_dependency import get_db
+
+router = APIRouter(prefix="/tasks")
+
+
+# GET TASK BY ID
+
+@router.get("/{task_id}", response_model=TaskSchema)
+async def get_task(task_id: int, session: Session = Depends(get_db)) -> TaskSchema:
+    searched_task = task.get_task_by_id(session, task_id)
+    if not searched_task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return searched_task
+
+
+# GET ALL TASKS
+@router.get("/", response_model=list[TaskSchema])
+async def get_all_tasks(session : Session = Depends(get_db)) -> List[TaskSchema]:
+    return task.get_all_tasks(session)
+
+
+# ADD NEW TASK
+
+@router.post("/create", response_model=TaskSchema)
+async def create_task(new_task:TaskCreateSchema , session : Session = Depends(get_db)) -> TaskSchema:
+    return task.create_task(session, new_task)
+
+
+# UPDATE TASK
+@router.put("/update/{task_id}", response_model=TaskSchema)
+async def update_task(task_id : int , change_task:TaskUpdateSchema , session : Session = Depends(get_db)) -> TaskSchema:
+    return task.update_task(session, task_id, change_task)
+
+
+# DELETING A TASK BY ID
+@router.delete("/delete/{task_id}", response_model=Dict[str, str])
+async def delete_task_by_id(task_id: int, session: Session = Depends(get_db)) -> Dict[str,str]:
+    deleted_task = task.delete_task(session, task_id)
+    if not deleted_task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return {"message": "Task deleted successfully"}
+
